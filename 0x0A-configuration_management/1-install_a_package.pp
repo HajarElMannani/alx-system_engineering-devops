a9bf3e5bc0a8d40968fa24401187f10be138e876
HajarElMannani <laso125@hotmail.com>
2025-01-10 12:56:39 -0800

    remove changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # installing flask from pip3
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  source   => '/usr/bin/',
 }
