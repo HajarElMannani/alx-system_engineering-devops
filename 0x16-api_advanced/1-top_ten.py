bb669366864c046b06a2f90f140a82877b611a7f
HajarElMannani <laso125@hotmail.com>
2025-02-25 13:39:41 -0800

    Pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-'''queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit'''
+'''queries the Reddit API and prints the titles of the first 10
+ hot posts listed for a given subreddit'''
 import requests
 
 
 def top_ten(subreddit):
-    '''Print the titles of the first 10 hot posts listed for a given subreddit'''
+    '''Print the titles of the first 10 hot posts listed
+    for a given subreddit'''
     url = 'https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10'
     headers = {'User-Agent': 'ALX-Project/1.0 by (u/Suspicious_Target472)'}
     resp = requests.get(url, headers=headers, allow_redirects=False)
