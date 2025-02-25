289b03b3b620ba2c64be2ccdad769a610dcb2457
HajarElMannani <laso125@hotmail.com>
2025-02-25 13:18:30 -0800

    Remove test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,6 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     headers = {'User-Agent': 'ALX-Project/1.0 by (u/Suspicious_Target472)'}
     resp = requests.get(url, headers=headers, allow_redirects=False)
-    print(resp.status_code) 
     if resp.status_code == 200:
         response = resp.json()
         return response['data'].get('subscribers')
