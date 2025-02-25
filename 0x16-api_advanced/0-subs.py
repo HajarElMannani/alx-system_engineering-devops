4954919e0877a1c6e19084332d50e0a85422eb81
HajarElMannani <laso125@hotmail.com>
2025-02-25 13:14:24 -0800

    chane code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     '''returns the number of all subscribers of Reddit\
      APIfor a given subreddit '''
-    headers = {'User-Agent': 'MyRedditBot/1.0 (by /u/Suspicious_Target472)'}
-    resp = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers, allow_redirects=False)
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    headers = {'User-Agent': 'ALX-Project/1.0 by (u/Suspicious_Target472)'}
+    resp = requests.get(url, headers=headers, allow_redirects=False)
     print(resp.status_code) 
     if resp.status_code == 200:
         response = resp.json()
