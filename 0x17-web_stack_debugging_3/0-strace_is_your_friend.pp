acb834531891f6d481e5fc53d5f33f1a3db15e8c
HajarElMannani <laso125@hotmail.com>
2025-02-26 21:19:43 +0000

    First commit

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # find out why Apache is returning a 500 error, fix it and then automate it using Puppet
 
 exec { 'wordpress-fix-php':
-command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-path    => '/bin/'
+command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+path    => '/usr/local/bin/:/usr/bin/:/bin/',
 }
 
