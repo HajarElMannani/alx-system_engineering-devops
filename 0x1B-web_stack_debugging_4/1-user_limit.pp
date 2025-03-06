1e0d0374553a343dbe7bf580ad222dee1b7ad177
HajarElMannani <laso125@hotmail.com>
2025-03-06 20:06:23 +0000

    First commit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+#increase user limit for opening files
+exec { 'increase holbertin hard limit':
+  command => 'sed -i "/holberton hard/s/5/5000" /etc/security/limits.conf',
+  path    => "/bin/"
+}
+
+exec { 'increase holbertin soft limit':
+command => 'sed -i "/holberton soft/s/4/4000" /etc/security/limits.
+conf',
+  path  => "/bin/"
+}
