3f8527d2a3c9f513092439625da482957a48c65a
HajarElMannani <laso125@hotmail.com>
2025-03-06 20:28:10 +0000

    add path

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 #increase user limit for opening files
 exec { 'holberton-hard-limit':
   command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/g" /etc/security/limits.conf',
-  path    => '/usr/bin/:/bin/',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 exec { 'holberton-soft-limit':
   command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/g" /etc/security/limits.conf',
-  path  => '/usr/bin/:/bin/',
+  path  => '/usr/local/bin/:/bin/',
 }
